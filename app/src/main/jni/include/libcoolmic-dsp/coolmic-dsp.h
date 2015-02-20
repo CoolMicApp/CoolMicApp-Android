bf5f7d787faa2a66283753b16d80e92f87c9b0ef
Philipp Schafft <lion@lion.leolix.org>
2015-02-20 11:08:45 +0000

    updated namespace to avoid futur collisions

--- app/src/main/jni/include/libcoolmic-dsp/coolmic-dsp.h
+++ app/src/main/jni/include/libcoolmic-dsp/coolmic-dsp.h
@@ -2,8 +2,8 @@
  *  Copyright (C) 2015      Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>
  */
 
-#ifndef __LIBCOOLMIC_DSP_H__
-#define __LIBCOOLMIC_DSP_H__
+#ifndef __COOLMIC_DSP_COOLMIC_DSP_H__
+#define __COOLMIC_DSP_COOLMIC_DSP_H__
 
 #define COOLMIC_DSP_CODEC_VORBIS "audio/ogg; codec=vorbis"
 #define COOLMIC_DSP_CODEC_OPUS   "audio/ogg; codec=opus"
