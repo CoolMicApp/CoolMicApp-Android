3b878f661e04139b191918f238d97cfa998bf2d4
Philipp Schafft <lion@lion.leolix.org>
2015-02-21 00:13:31 +0000

    Added libshout interface

--- app/src/main/jni/include/libcoolmic-dsp/shout.h
+++ app/src/main/jni/include/libcoolmic-dsp/shout.h
@@ -19,10 +19,31 @@
 /* forward declare internally used structures */
 typedef struct coolmic_shout coolmic_shout_t;
 
+/* public structures */
+typedef struct coolmic_shout_config {
+    /* connection settings */
+    const char *hostname;    /* server's hostname */
+    int         port;        /* server's port number */
+    int         tlsmode;     /* TLS mode to use (SHOUT_TLS_*) */
+    const char *cadir;       /* CA directory (default: NULL) */
+    const char *cafile;      /* CA file (default: NULL) */
+
+    /* mount point settings */
+    const char *mount;       /* mount point name */
+
+    /* credentials */
+    const char *username;    /* username to login as (default: NULL) */
+    const char *password;    /* password to use with username */
+    const char *client_cert; /* client cerificate (TLS) (default: NULL=none) */
+} coolmic_shout_config_t;
+
 /* Management of the encoder object */
-coolmic_shout_t *coolmic_shout_new(...);
+coolmic_shout_t *coolmic_shout_new(void);
 int              coolmic_shout_ref(coolmic_shout_t *self);
 int              coolmic_shout_unref(coolmic_shout_t *self);
+
+/* configure remote connection */
+int              coolmic_shout_set_config(coolmic_shout_t *self, const coolmic_shout_config_t *conf);
 
 /* This is to attach the IO Handle of the Ogg data stream that is to be passed to the Icecast server */
 int              coolmic_shout_attach_iohandle(coolmic_shout_t *self, coolmic_iohandle_t *handle);
---
+++ app/src/main/jni/libcoolmic-dsp/shout.c
@@ -0,0 +1,134 @@
+/*
+ *  Copyright (C) 2015      Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>
+ */
+
+/* Please see the corresponding header file for details of this API. */
+
+#include <stdlib.h>
+#include <shout/shout.h>
+#include <libcoolmic-dsp/shout.h>
+
+struct coolmic_shout {
+    size_t refc;
+    shout_t *shout;
+    coolmic_iohandle_t *in;
+};
+
+coolmic_shout_t *coolmic_shout_new(void)
+{
+    coolmic_shout_t *ret = calloc(1, sizeof(coolmic_shout_t));
+    if (!ret)
+        return NULL;
+
+    shout_init();
+
+    ret->refc = 1;
+    ret->shout = shout_new();
+    if (!ret->shout) {
+        free(ret);
+        shout_shutdown();
+        return NULL;
+    }
+
+    /* set some stuff that is always the same for all connections */
+    shout_set_protocol(ret->shout, SHOUT_PROTOCOL_HTTP);
+    shout_set_format(ret->shout, SHOUT_FORMAT_OGG);
+
+    return ret;
+}
+
+int              coolmic_shout_set_config(coolmic_shout_t *self, const coolmic_shout_config_t *conf)
+{
+    if (!self || !conf)
+        return -1;
+
+    if (shout_set_host(self->shout, conf->hostname) != SHOUTERR_SUCCESS)
+        return -1;
+
+    if (shout_set_port(self->shout, conf->port) != SHOUTERR_SUCCESS)
+        return -1;
+
+    if (shout_set_tls(self->shout, conf->tlsmode) != SHOUTERR_SUCCESS)
+        return -1;
+
+    if (conf->cadir)
+        if (shout_set_ca_directory(self->shout, conf->cadir) != SHOUTERR_SUCCESS)
+            return -1;
+
+    if (conf->cafile)
+        if (shout_set_ca_file(self->shout, conf->cafile) != SHOUTERR_SUCCESS)
+            return -1;
+
+    if (shout_set_mount(self->shout, conf->mount) != SHOUTERR_SUCCESS)
+        return -1;
+
+    if (conf->username)
+        if (shout_set_user(self->shout, conf->username) != SHOUTERR_SUCCESS)
+            return -1;
+
+    if (shout_set_password(self->shout, conf->password) != SHOUTERR_SUCCESS)
+        return -1;
+
+    if (conf->client_cert)
+        if (shout_set_client_certificate(self->shout, conf->client_cert) != SHOUTERR_SUCCESS)
+            return -1;
+
+    return 0;
+}
+
+int              coolmic_shout_ref(coolmic_shout_t *self)
+{
+    if (!self)
+        return -1;
+    self->refc++;
+    return 0;
+}
+
+int              coolmic_shout_unref(coolmic_shout_t *self)
+{
+    if (!self)
+        return -1;
+    self->refc--;
+
+    if (self->refc)
+        return 0;
+
+    shout_close(self->shout);
+    shout_free(self->shout);
+    coolmic_iohandle_unref(self->in);
+
+    free(self);
+
+    shout_shutdown();
+
+    return 0;
+}
+
+int              coolmic_shout_attach_iohandle(coolmic_shout_t *self, coolmic_iohandle_t *handle)
+{
+    if (!self)
+        return -1;
+    if (self->in)
+        coolmic_iohandle_unref(self->in);
+    /* ignore errors here as handle is allowed to be NULL */
+    coolmic_iohandle_ref(self->in = handle);
+    return 0;
+}
+
+int              coolmic_shout_iter(coolmic_shout_t *self)
+{
+    char buffer[1024];
+    ssize_t ret;
+
+    if (shout_get_connected(self->shout) == SHOUTERR_UNCONNECTED)
+        if (shout_open(self->shout) != SHOUTERR_SUCCESS)
+            return -1;
+
+    ret = coolmic_iohandle_read(self->in, buffer, sizeof(buffer));
+    if (ret > 0)
+        shout_send(self->shout, (void*)buffer, ret);
+
+    shout_sync(self->shout);
+
+    return 0;
+}
