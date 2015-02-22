3831118353dad75197873bc561102adcfaa76d0f
Stephan Jauernick <info@stephan-jauernick.de>
2015-02-22 20:57:33 +0100

    adapted the android ndk buildfiles

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -5,7 +5,7 @@ LOCAL_C_INCLUDE := $(LOCAL_PATH)/include
 include $(addprefix $(LOCAL_PATH)/, $(addsuffix /Android.mk, \
 	libogg \
 	libvorbis \
-	libvorbis-jni \
+    libcoolmic-dsp \
 ))
 
 
---
+++ app/src/main/jni/libcoolmic-dsp/Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := coolmic-dsp
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/../include -fsigned-char
+LOCAL_CFLAGS += -march=armv6 -marm -mfloat-abi=softfp -mfpu=vfp
+
+LOCAL_SHARED_LIBRARIES := libogg libvorbis libshout
+
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog -lshout -L$(LOCAL_PATH)/../libshout/src/.libs/
+
+LOCAL_SRC_FILES := \
+	coolmic-dsp.c \
+	enc.c \
+	iohandle.c \
+	shout.c \
+	simple.c \
+	snddev.c \
+	snddev_null.c\
+
+include $(BUILD_SHARED_LIBRARY)
