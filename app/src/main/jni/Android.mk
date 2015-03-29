5b0c6ff11c8c730494357597246038ebfb49b5d4
Stephan Jauernick <info@stephan-jauernick.de>
2015-03-29 23:51:18 +0200

    use newer libshout compilation mechanism to enable true multiplattform useability

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,8 +3,8 @@ LOCAL_PATH := $(call my-dir)
 LOCAL_C_INCLUDE := $(LOCAL_PATH)/include
 
 include $(addprefix $(LOCAL_PATH)/, $(addsuffix /Android.mk, \
-    libshout-newer \
-	libogg \
+    libshout-new \
+    libogg \
 	libvorbis \
     libcoolmic-dsp \
     libcoolmic-dsp-java \
