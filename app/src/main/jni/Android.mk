fceeed99e912f897f467493cec9f112c43c15394
Stephan Jauernick <info@stephan-jauernick.de>
2016-02-03 20:29:04 +0100

    Added files for building the newest libshout

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 LOCAL_C_INCLUDE := $(LOCAL_PATH)/include
 
 include $(addprefix $(LOCAL_PATH)/, $(addsuffix /Android.mk, \
-    libshout-new \
+    libshout \
     libogg \
 	libvorbis \
     libcoolmic-dsp \
