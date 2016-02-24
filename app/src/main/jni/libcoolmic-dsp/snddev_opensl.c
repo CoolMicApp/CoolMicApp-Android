13460ed27d2d9f0311617d45bad71e3cb5a07f02
Philipp Schafft <lion@lion.leolix.org>
2016-02-24 16:05:34 +0000

    Update: Set license and copyright headers.

--- app/src/main/jni/libcoolmic-dsp/snddev_opensl.c
+++ app/src/main/jni/libcoolmic-dsp/snddev_opensl.c
@@ -1,6 +1,25 @@
-/* %%%COPYRIGHT%%% */
+/*
+ *      Copyright (C) Jordan Erickson                     - 2014-2016,
+ *      Copyright (C) Löwenfelsen UG (haftungsbeschränkt) - 2015-2016
+ *       on behalf of Jordan Erickson.
+ */
 
-/* %%%LICENSE%%% */
+/*
+ * This file is part of Cool Mic.
+ * 
+ * Cool Mic is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Cool Mic is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Cool Mic.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* This is a dummy sound driver. It supports record and playback.
  * In record mode it will read as zeros (silence).
