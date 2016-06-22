61cf97e97718985602f07b093438bf82943b1a12
Stephan Jauernick <stjauernick@de.loewenfelsen.net>
2016-06-22 19:25:36 +0200

    VU-Meter: add setter for array fields to result class

--- app/src/main/java/cc/echonet/coolmicdspjava/VUMeterResult.java
+++ app/src/main/java/cc/echonet/coolmicdspjava/VUMeterResult.java
@@ -19,4 +19,20 @@ public class VUMeterResult {
 
     public int[] channels_peak;
     public double[] channels_power;
+
+    public void setChannelPeakPower(int channel, int peak, double power)
+    {
+        if(this.channels_peak == null)
+        {
+            this.channels_peak = new int[16];
+        }
+
+        if(this.channels_power == null)
+        {
+            this.channels_power = new double[16];
+        }
+
+        this.channels_peak[channel] = peak;
+        this.channels_power[channel] = power;
+    }
 }
