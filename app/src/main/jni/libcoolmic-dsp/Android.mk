47f36edaa8e32d92e21e0fb04a5f10dcae42c314
Philipp Schafft <phschafft@de.loewenfelsen.net>
2016-07-25 07:19:58 +0000

    Updated submodules: libcoolmic-dsp.

--- app/src/main/jni/libcoolmic-dsp/Android.mk
+++ app/src/main/jni/libcoolmic-dsp/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := coolmic-dsp
-LOCAL_CFLAGS += -I$(LOCAL_PATH)/../include -I$(LOCAL_PATH)/libcoolmic-dsp/include -fsigned-char -DHAVE_SNDDRV_DRIVER_OPENSL
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/../include -I$(LOCAL_PATH)/libcoolmic-dsp/include -fsigned-char -DHAVE_SNDDRV_DRIVER_OPENSL -DHAVE_SNDDRV_DRIVER_STDIO
 #LOCAL_CFLAGS += -march=armv6 -marm -mfloat-abi=softfp -mfpu=vfp
 
 LOCAL_SHARED_LIBRARIES := libogg libvorbis libshout
@@ -12,7 +12,7 @@ LOCAL_SHARED_LIBRARIES := libogg libvorbis libshout
 
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog -lOpenSLES
 
-SOURCE_FILES = coolmic-dsp.c enc.c iohandle.c shout.c simple.c snddev.c vumeter.c tee.c snddev_null.c snddev_opensl.c util.c
+SOURCE_FILES = coolmic-dsp.c enc.c iohandle.c shout.c simple.c snddev.c vumeter.c tee.c snddev_null.c snddev_opensl.c snddev_stdio.c util.c metadata.c
 
 LOCAL_SRC_FILES := $(foreach c,$(SOURCE_FILES),libcoolmic-dsp/src/$c)
 
