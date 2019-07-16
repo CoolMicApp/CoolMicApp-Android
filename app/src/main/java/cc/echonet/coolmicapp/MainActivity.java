0db547b543cfbf44b8ea32f140e99b8049e2bea1
Philipp Schafft <phschafft@de.loewenfelsen.net>
2019-07-16 06:26:30 +0000

    Cleanup: Removed unused import

--- app/src/main/java/cc/echonet/coolmicapp/MainActivity.java
+++ app/src/main/java/cc/echonet/coolmicapp/MainActivity.java
@@ -31,7 +31,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
