b021c565d68fdb9e02bce39eedb0a898db49ae52
Philipp Schafft <phschafft@de.loewenfelsen.net>
2020-02-12 22:34:24 +0000

    Update: Updated copyright statements

--- app/src/main/java/cc/echonet/coolmicapp/Icecast/Response/Stats.java
+++ app/src/main/java/cc/echonet/coolmicapp/Icecast/Response/Stats.java
@@ -1,3 +1,25 @@
+/*
+ *      Copyright (C) Jordan Erickson                     - 2014-2020,
+ *      Copyright (C) Löwenfelsen UG (haftungsbeschränkt) - 2015-2020
+ *       on behalf of Jordan Erickson.
+ *
+ * This file is part of Cool Mic.
+ *
+ * Cool Mic is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cool Mic is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cool Mic.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package cc.echonet.coolmicapp.Icecast.Response;
 
 public class Stats extends Response {
--- app/src/main/java/cc/echonet/coolmicapp/MainActivity.java
+++ app/src/main/java/cc/echonet/coolmicapp/MainActivity.java
@@ -1,10 +1,8 @@
 /*
- *      Copyright (C) Jordan Erickson                     - 2014-2016,
- *      Copyright (C) Löwenfelsen UG (haftungsbeschränkt) - 2015-2016
+ *      Copyright (C) Jordan Erickson                     - 2014-2020,
+ *      Copyright (C) Löwenfelsen UG (haftungsbeschränkt) - 2015-2020
  *       on behalf of Jordan Erickson.
- */
-
-/*
+ *
  * This file is part of Cool Mic.
  *
  * Cool Mic is free software: you can redistribute it and/or modify
@@ -19,7 +17,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Cool Mic.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
+
 package cc.echonet.coolmicapp;
 
 import android.app.Activity;
