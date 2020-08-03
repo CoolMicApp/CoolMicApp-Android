b499ca59614211d925a5ab424911bb3bd0430a2a
Philipp Schafft <phschafft@de.loewenfelsen.net>
2020-08-03 20:43:13 +0000

    Feature: Added Javadoc to Wrapper

--- app/src/main/java/cc/echonet/coolmicdspjava/Wrapper.java
+++ app/src/main/java/cc/echonet/coolmicdspjava/Wrapper.java
@@ -28,7 +28,16 @@ import androidx.annotation.NonNull;
 import java.io.Closeable;
 
 /**
- * Created by stephanj on 2/22/15.
+ * This class wraps libcoolmic-dsp's simple API.
+ * <P>
+ * The general workflow consists of calling:
+ * <ul>
+ *     <li>{@link #prepare(CallbackHandler, String, int, String, String, String, String, int, int, int)} to prepare the stream.</li>
+ *     <li>Optionally call setters for additional settings.</li>
+ *     <li>{@link #start()} to start recording.</li>
+ *     <li>Optionally call setters for updating the stream or {@link #nextSegment(InputStreamAdapter)} to start a new segment.</li>
+ *     <li>{@link #close()} when done.</li>
+ * </ul>
  */
 public final class Wrapper implements Closeable {
     private static @NonNull WrapperConstants.WrapperInitializationStatus state;
@@ -53,32 +62,106 @@ public final class Wrapper implements Closeable {
         }
     }
 
+    /**
+     * Gets the state of the object.
+     * This should be {@link WrapperConstants.WrapperInitializationStatus#WRAPPER_INTITIALIZED}.
+     * @return The state.
+     */
     public synchronized @NonNull WrapperConstants.WrapperInitializationStatus getState() {
         return state;
     }
 
+    /**
+     * Gets the exception cough when trying to initialise.
+     * @return The exception or {@code null}.
+     */
     public synchronized @Nullable Throwable getInitException() {
         return initException;
     }
 
+    /**
+     * Prepares the wrapper for streaming.
+     *
+     * @param handler The callback handler used for updates from the library to the Java world.
+     * @param hostname The hostname to connect to.
+     * @param port The port to connect to.
+     * @param username The username to connect with.
+     * @param password The password to connect with.
+     * @param mount The mount point to connect to.
+     * @param codec The codec to use.
+     * @param rate The sample rate to use.
+     * @param channels The number of channels to use.
+     * @param buffersize The buffer site to use.
+     * @return TODO
+     */
+    public synchronized native int prepare(CallbackHandler handler, String hostname, int port, String username, String password, String mount, String codec, int rate, int channels, int buffersize);
+
+    /**
+     * This starts streaming.
+     * @return TODO
+     */
     public synchronized native int start();
 
+    /**
+     * This stops streaming and closes the connection.
+     */
     @Override
     public synchronized native void close();
 
+    /**
+     * Gets the state of the wrapper.
+     * Will return true once {@link #prepare(CallbackHandler, String, int, String, String, String, String, int, int, int)}
+     * was called and {@link #close()} has not yet been called.
+     *
+     * @return Whether the wrapper is in prepared state.
+     */
     public synchronized native boolean isPrepared();
 
+    /**
+     * Sets the interval for generating VU-Meter events.
+     * @param interval The interval [magic] or zero to disable.
+     * @return TODO
+     */
     public synchronized native int setVuMeterInterval(int interval);
 
+    /**
+     * Updates the metadata and quality settings for the stream and applies those parameters.
+     * @param title The new title to use (Vorbis Comment Tag: TITLE).
+     * @param artist The new artist to use (Vorbis Comment Tag: ARTIST).
+     * @param quality The new quality to use in the range of [-0.1:+1.0].
+     * @param restart Whether the settings should be applied.
+     * @return TODO
+     */
     public synchronized native int performMetaDataQualityUpdate(String title, String artist, double quality, int restart);
 
+    /**
+     * Sets the profile for reconnecting to the server in case the connection is lost.
+     * @param profile The profile to use or {@code "disabled"}, {@code "enabled"}, or {@code "default"}.
+     * @return TODO
+     */
     public synchronized native int setReconnectionProfile(String profile);
 
+    /**
+     * Sets the master gain.
+     * @param scale The base unit to use of {@code gain}.
+     * @param gain The value to set the master gain for all channels to in [scale].
+     * @return TODO
+     */
     public synchronized native int setMasterGain(int scale, int gain);
 
+    /**
+     * Sets the master gain.
+     * @param scale The base unit to use of {@code gain}.
+     * @param gainLeft The value to set the master gain for the left channel to in [scale].
+     * @param gainRight The value to set the master gain for the right channel to in [scale].
+     * @return TODO
+     */
     public synchronized native int setMasterGain(int scale, int gainLeft, int gainRight);
 
+    /**
+     * Schedules and switches to the next segment.
+     * @param inputStreamAdapter The adapter for the next segment or {@code null} for a live segment.
+     * @return TODO
+     */
     public synchronized native int nextSegment(InputStreamAdapter inputStreamAdapter);
-
-    public synchronized native int prepare(CallbackHandler handler, String hostname, int port, String username, String password, String mount, String codec, int rate, int channels, int buffersize);
 }
