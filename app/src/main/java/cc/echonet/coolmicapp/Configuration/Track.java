54f821337e6900f3c22d2cb1b85543e62b64b70e
Philipp Schafft <phschafft@de.loewenfelsen.net>
2020-12-02 08:49:55 +0000

    Feature: Added API for accessing track metadata via key strings

--- app/src/main/java/cc/echonet/coolmicapp/Configuration/Track.java
+++ app/src/main/java/cc/echonet/coolmicapp/Configuration/Track.java
@@ -22,11 +22,47 @@
 
 package cc.echonet.coolmicapp.Configuration;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class Track extends ProfileBase {
     Track(@NotNull ProfileBase profile) {
         super(profile);
+    }
+
+    public @NotNull List<String> getKeys() {
+        final @NotNull List<String> ret = new ArrayList<>();
+        ret.add("artist");
+        ret.add("title");
+        return ret;
+    }
+
+    @Contract("_, !null -> !null; _, null -> _")
+    public @Nullable String getValue(@NotNull String key, @Nullable String def) {
+        final @Nullable String ret;
+
+        switch (key.toLowerCase(Locale.ROOT)) {
+            case "artist": ret = getArtist(); break;
+            case "title": ret = getTitle(); break;
+            default: ret = null; break;
+        }
+
+        if (ret == null || ret.isEmpty())
+            return def;
+
+        return ret;
+    }
+
+    public void setValue(@NotNull String key, @Nullable String value) {
+        if (!(key.equals("artist") || key.equals("title")))
+            return;
+
+        editor.putString("general_" + key, value);
     }
 
     public String getArtist() {
